30f44ba489baec78414e23298208c93536990ae4
Julian QG <31473821+JulianQG@users.noreply.github.com>
2020-04-27 16:24:57 +0100

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -6,35 +6,35 @@ using Swashbuckle.AspNetCore.Swagger;
 
 namespace TodoApi
 {
-    public class Startup
-    {       
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoList"));
-            services.AddMvc();
+   public class Startup
+   {
+      public void ConfigureServices(IServiceCollection services)
+      {
+         services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoList"));
+         services.AddMvc();
 
-            // Register the Swagger generator, defining one or more Swagger documents
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
-            });
-        }
+         // Register the Swagger generator, defining one or more Swagger documents
+         services.AddSwaggerGen(c =>
+         {
+            c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
+         });
+      }
 
-        public void Configure(IApplicationBuilder app)
-        {
-            // Enable middleware to serve generated Swagger as a JSON endpoint.
-            app.UseSwagger();
+      public void Configure(IApplicationBuilder app)
+      {
+         // Enable middleware to serve generated Swagger as a JSON endpoint.
+         app.UseSwagger();
 
-            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), specifying the Swagger JSON endpoint.
-            app.UseSwaggerUI(c =>
-            {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
-            });
+         // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), specifying the Swagger JSON endpoint.
+         app.UseSwaggerUI(c =>
+         {
+            c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+         });
 
-            app.UseDefaultFiles();
-            app.UseStaticFiles();
+         app.UseDefaultFiles();
+         app.UseStaticFiles();
 
-            app.UseMvc();
-        }
-    }
+         app.UseMvc();
+      }
+   }
 }
